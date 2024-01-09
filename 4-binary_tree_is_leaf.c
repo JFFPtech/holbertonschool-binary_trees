6ec6de86d2e435ae09cf81eff5b2004d4e0df4c2
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-01-09 07:43:24 -0400

    finished adding main and prototype for 4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - checks if node is a leaf of a binary tree
+ * @node: pointer to the node
+ *
+ * Return: 1 if leaf, 0 if not leaf
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+    if (node == NULL || (node->left != NULL || node->right != NULL))
+        return 0;
+
+    return 1;
+}
+
