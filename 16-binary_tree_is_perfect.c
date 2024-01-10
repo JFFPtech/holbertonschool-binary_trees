a9d0b6080cdf1c296a1c27a61e37e65c628527f3
LNCarrion <lestycarrion@gmail.com>
2024-01-10 10:43:16 -0400

    fix 16, code input

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -9,25 +9,14 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    int left = 0, right = 0;
 
-    if (tree == NULL)
+    if ((tree->left == NULL) != (tree->right == NULL))
         return (0);
-
-    if (tree->left != NULL)
-        left = binary_tree_is_perfect(tree->left);
-
-    if (tree->right != NULL)
-        right = binary_tree_is_perfect(tree->right);
-
-    if ((left == 0 && right != 0) || (left != 0 && right == 0))
+    if (tree->left == NULL)
+        return (1);
+    if ((tree->left->left == NULL) != (tree->right->left == NULL))
         return (0);
-
-    if (left == 0 && right == 0)
-        return (1);
-
-    if (left == right)
-        return (1);
-
-    return (0);
+    if (!binary_tree_is_perfect(tree->left) || !binary_tree_is_perfect(tree->right))
+        return (0);
+    return (1);
 }
